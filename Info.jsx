b9eeb2720a419b12e3555d9aa1bdf27f64406b29
Rishikesh Sonawane <84461892+rishikesh-sonawane@users.noreply.github.com>
2021-06-26 18:19:09 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>Enroll For Internship</h1>
+      <p>
+        Go to the ShapeAI website And enroll for Full-Stack Web Development
+        Internship.
+      </p>
+    </div>
+  );
+}
+
+export default Info;
